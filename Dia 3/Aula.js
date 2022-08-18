797638190a998df9b5d61b1a4bc65b79a6c46807
Diego Hermys <diegohermys@gmail.com>
2022-08-18 15:15:05 -0300

    Miniprojeto 3 Curso JavaScript

--- Dia 3/Aula.js
+++ Dia 3/Aula.js
@@ -14,9 +14,27 @@ console.log(b); */
 
 // Mini-projeto 2
 
-let valorMaior = max(5000, 6000);
+/* let valorMaior = max(5000, 6000);
 console.log(valorMaior);
 
 function max(num1, num2) {
     return num1 > num2 ? num1 : num2;
+} */
+
+// Mini-projeto 3
+
+const resultado = fizzBuzz(20);
+console.log(resultado);
+
+function fizzBuzz(entrada) {
+    if (typeof entrada !== 'number')
+        return 'Não é um número';
+    if ((entrada % 3 === 0) && (entrada % 5 === 0))
+        return 'FizzBuzz';
+    if (entrada % 3 === 0)
+        return 'Fizz';
+    if (entrada % 5 === 0)
+        return 'Buzz';
+
+    return entrada;
 }
