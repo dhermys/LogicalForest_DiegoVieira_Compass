31972098c3e0413a6d96f8f1a77179c54f084c1c
Diego Hermys <diegohermys@gmail.com>
2022-08-19 11:03:10 -0300

    Miniprojeto 12 Curso JavaScript

--- Dia 3/Aula.js
+++ Dia 3/Aula.js
@@ -164,7 +164,7 @@ function numeroPrimo(numero) {
 
 // Mini-projeto 11
 
-let endereco = {
+/* let endereco = {
     rua: 'a',
     cidade: 'b',
     cep: 'c'
@@ -175,4 +175,30 @@ function exibirEndereco(endereco) {
     console.log(chave, endereco[chave]);
 }
 
-exibirEndereco(endereco);
+exibirEndereco(endereco); */
+
+// Mini-projeto 12
+
+function Endereco(rua, cidade, cep) {
+    this.rua = rua,
+        this.cidade = cidade,
+        this.cep = cep
+}
+
+const endereco1 = new Endereco('a', 'b', 'c');
+const endereco2 = new Endereco('a', 'b', 'c');
+const endereco3 = endereco1;
+
+function saoIguais(endereco1, endereco2) {
+    return endereco1.rua === endereco2.rua &&
+        endereco1.cidade === endereco2.cidade &&
+        endereco1.cep === endereco2.cep
+}
+
+console.log(saoIguais(endereco1, endereco2));
+
+function temEnderecoMemoriaIguais(endereco1, endereco2) {
+    return endereco1 === endereco3
+}
+
+console.log(temEnderecoMemoriaIguais(endereco1, endereco2));
