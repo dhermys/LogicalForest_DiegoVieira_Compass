f1638c74dcaaa9bfcd2305e412521f77cf514092
Diego Hermys <diegohermys@gmail.com>
2022-08-19 11:38:56 -0300

    Miniprojeto 14 Curso JavaScript

--- Dia 3/Aula.js
+++ Dia 3/Aula.js
@@ -205,7 +205,7 @@ console.log(temEnderecoMemoriaIguais(endereco1, endereco2)); */
 
 // Mini-projeto 13
 
-let postagem = {
+/* let postagem = {
     titulo: 'a',
     mensagem: 'b',
     autor: 'c',
@@ -217,4 +217,42 @@ let postagem = {
     estaAoVivo : true
 }
 
-console.log(postagem);
+console.log(postagem); */
+
+// Mini-projeto 14
+
+let faixas = [
+    { tooltip: 'até R$ 700', minimo: 0, maximo: 700 },
+    { tooltip: 'de R$ 700 a R$ 1000', minimo: 700, maximo: 1000 },
+    { tooltip: 'R$ 1000 ou mais', minimo: 1000, maximo: 9999999 }
+];
+
+function criaFaixaPreco(tooltip, minimo, maximo) {
+    return {
+        tooltip,
+        minimo,
+        maximo
+    }
+};
+
+let faixas2 = [
+    criaFaixaPreco('a', 1, 100),
+    criaFaixaPreco('b', 100, 1000),
+    criaFaixaPreco('a', 1000, 10000),
+];
+
+function FaixaPreco(tooltip, minimo, maximo) {
+    this.tooltip = tooltip,
+    this.minimo = minimo,
+    this.maximo = maximo
+};
+
+let faixas3 = [
+    new FaixaPreco('d', 10, 20),
+    new FaixaPreco('e', 20, 30),
+    new FaixaPreco('f', 30, 40)
+];
+
+console.log(faixas);
+console.log(faixas2);
+console.log(faixas3);
