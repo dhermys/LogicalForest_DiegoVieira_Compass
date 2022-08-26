15991f9c1bfe4fff6646dfac24e6553e1e2999ab
Diego Hermys <diegohermys@gmail.com>
2022-08-26 14:59:23 -0300

    Criação arquivo projetocalculadora.txt e operações de adição e subtração

--- calculadora/test/calculadora.spec.js
+++ calculadora/test/calculadora.spec.js
@@ -4,7 +4,7 @@ import Calculadora from '../src/calculadora.js'
 
 const expect = chai.expect
 
-describe('Testes de soma', () => {
+describe('Testes de adição', () => {
     it('Deve somar 4 e 5 resultando em 9', () => {
         let resultado = Calculadora.soma(4, 5)
         expect(resultado).to.be.eq(9)
@@ -13,12 +13,32 @@ describe('Testes de soma', () => {
         let resultado = Calculadora.soma(-4, 5)
         expect(resultado).to.be.eq(1)
     })
+    it('Deve somar -4 e -5 resultando em -9', () => {
+        let resultado = Calculadora.soma(-4, -5)
+        expect(resultado).to.be.eq(-9)
+    })
+    it('Deve somar 4.5 e 4.5 resultando em 9', () => {
+        let resultado = Calculadora.soma(4.5, 4.5)
+        expect(resultado).to.be.eq(9)
+    })
   })
 
   describe('Testes de subtração', () => {
     it('Deve subtrair 4 e 5 resultando em -1', () => {
         let resultado = Calculadora.sub(4, 5)
         expect(resultado).to.be.eq(-1)
+    })
+    it('Deve subtrair -4 e 5 resultando em -9', () => {
+        let resultado = Calculadora.sub(-4, 5)
+        expect(resultado).to.be.eq(-9)
+    })
+    it('Deve subtrair -4 e -5 resultando em 1', () => {
+        let resultado = Calculadora.sub(-4, -5)
+        expect(resultado).to.be.eq(1)
+    })
+    it('Deve subtrair 4.5 e 4.5 resultando em 0', () => {
+        let resultado = Calculadora.sub(4.5, 4.5)
+        expect(resultado).to.be.eq(0)
     })
   })
 
